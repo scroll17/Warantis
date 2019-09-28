579cf648e8ec732e23db50c2747abab8e5217e1d
Denis Zolotarenko <zololotarenko.2015@gmail.com>
2019-09-28 13:53:22 +0300

    buid

---
+++ build/precache-manifest.e64d6aaa7c8beda63d96f8c58af1855a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d54147c098679e16e29774d99c898a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fe32fff903ac377b88f0",
+    "url": "/static/css/main.c3ee4d8d.chunk.css"
+  },
+  {
+    "revision": "25be65cc72dcf43e878e",
+    "url": "/static/js/2.a794ec2a.chunk.js"
+  },
+  {
+    "revision": "fe32fff903ac377b88f0",
+    "url": "/static/js/main.5a92141a.chunk.js"
+  },
+  {
+    "revision": "c13b23139526b2bc583f",
+    "url": "/static/js/runtime-main.3ad6166a.js"
+  },
+  {
+    "revision": "6bb0f17800ad81539298a62454f3cacd",
+    "url": "/static/media/bg-bitcoin.6bb0f178.png"
+  },
+  {
+    "revision": "ffdecb4c484d1cd08d6de8e37184e156",
+    "url": "/static/media/calc-bg.ffdecb4c.png"
+  },
+  {
+    "revision": "e69801dc7f3874eb7e624a253a18b6c8",
+    "url": "/static/media/header-logo.e69801dc.svg"
+  },
+  {
+    "revision": "679313c27021ee22f24e0ccabf71cb95",
+    "url": "/static/media/how-work-01.679313c2.svg"
+  },
+  {
+    "revision": "f8522ea696466d332ecad089fc6dc8cf",
+    "url": "/static/media/icon-etherium.f8522ea6.svg"
+  },
+  {
+    "revision": "8afc9c8aa3ca498e27b5ec1f2820d98c",
+    "url": "/static/media/icon-litecoin.8afc9c8a.svg"
+  },
+  {
+    "revision": "9a2f1f518e50a0c28ccc412f49dac363",
+    "url": "/static/media/icon-security-pink.9a2f1f51.svg"
+  },
+  {
+    "revision": "8b6f41d95a3af3ecbf8fdb1d81e26a20",
+    "url": "/static/media/icon-security.8b6f41d9.svg"
+  },
+  {
+    "revision": "4cb7f94897b5c0f00867d5e44a61a26a",
+    "url": "/static/media/icon-up-green.4cb7f948.svg"
+  },
+  {
+    "revision": "974122a224fa61474e99cd1c5305dfae",
+    "url": "/static/media/liken-logic.974122a2.svg"
+  },
+  {
+    "revision": "f35d417cdf80514d2cb742a60b193ff0",
+    "url": "/static/media/mountain.f35d417c.png"
+  },
+  {
+    "revision": "6976be6808281916afe05ab008ba7853",
+    "url": "/static/media/stars.6976be68.svg"
+  },
+  {
+    "revision": "c98c1b1f732d01415aaf85c382e4da6b",
+    "url": "/static/media/women.c98c1b1f.png"
+  }
+]);
